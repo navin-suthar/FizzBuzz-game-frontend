8b171752869cfd7f69a64e2f0cb444a2acba0d8a
Navin Solanki <navinsukanraj@gmail.com>
2024-05-22 11:25:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('/fizzbuzz', {
+        values: input.split(',').map(value => parseInt(value.trim(), 10))
+      });
+      setOutput(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input type="text" placeholder='Enter  values' value={input} onChange={handleInputChange} />
+      <button onClick={handleSubmit}>Submit</button>
+      <div >
+        <h2>Output:</h2>
+        <ul>
+          {output.map((item, index) => (
+            <li key={index} style={{ listStyle: item === '' ? 'initial' : 'none' }}>
+               {item.result}
+              <ul>
+                {item.divisions.map((division, idx) => (
+                  <li key={idx}>{division}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
